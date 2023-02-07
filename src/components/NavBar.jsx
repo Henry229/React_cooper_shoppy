58ad43bb3fda74629736b9ccdad68c9d260d2731
Henry Chun <chunhyungjoon@gmail.com>
2023-02-07 17:03:47 +1100

    add logout and onUserStateChange on navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiShoppingBag } from 'react-icons/fi';
 import { BsFillPencilFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import { login, logout, onUserStateChange } from '../api/firebase';
+import { useEffect } from 'react';
+import User from './User';
 
 const NavBar = () => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  const handleLogin = () => {
+    // equivalent to .then(setUser) 양쪽에 user가 있으니까 생략해도 됨
+    login().then((user) => {
+      setUser(user);
+    });
+  };
+
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
+
   return (
-    <header className='flex justify-between'>
-      <Link to='/'>
+    <header className='flex justify-between border-b border-gray-300 p-2'>
+      <Link to='/' className='flex items-center text-4xl text-purple-500 gap-1'>
         <FiShoppingBag />
         <h1>Shoppy</h1>
       </Link>
-      <nav>
+      <nav className='flex items-center gap-4 font-semibold'>
         <Link to='/products'>Products</Link>
         <Link to='/carts'>Carts</Link>
-        <Link to='/products/new'>
+        <Link to='/products/new' className='text-2xl'>
           <BsFillPencilFill />
         </Link>
-        <button>Login</button>
+        {user && <User user={user} />}
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
       </nav>
     </header>
   );
